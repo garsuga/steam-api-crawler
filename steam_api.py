fceffb04e79ab31d6446dc99bfcccbfa1ef11c56
garsuga <gsugarbaker@gmail.com>
2019-12-30 21:24:28 -0600

    Initial Commit
    Cleanup and upload of an old project I made

---
+++ steam_api.py
@@ -0,0 +1,103 @@
+import json
+import requests
+import time
+
+# API Time variables
+# maxRequestsPerFiveMinutes = 200
+# maxRequestsPerSecond = 200 / (5 * 60)
+delay = 1.5 * 1.1
+# delay = delay * 1.1
+lastTime = time.time()
+
+
+# API Time helper function
+def api_wait():
+    global lastTime
+    time.sleep(max(0, int(delay - (time.time() - lastTime))))
+    lastTime = time.time()
+
+
+class Friend:
+    def __init__(self, dict):
+        self.steamid = str(dict['steamid']).strip()
+        self.friend_since = str(dict['friend_since']).strip()
+
+
+class Game:
+    def __init__(self, dict):
+        self.appid = str(dict['appid']).strip()
+        self.name = str(dict['name']).strip()
+        self.img_icon_url = 'http://media.steampowered.com/steamcommunity/public/images/apps/%s/%s.jpg' \
+                       % (str(self.appid).strip(), str(dict['img_icon_url']).strip()) if 'img_icon_url' in dict else ''
+        self.img_logo_url = 'http://media.steampowered.com/steamcommunity/public/images/apps/%s/%s.jpg' \
+                       % (str(self.appid).strip(), str(dict['img_logo_url']).strip()) if 'img_logo_url' in dict else ''
+        self.has_community_visible_stats = bool(str(dict['has_community_visible_stats']).strip()) if 'has_community_visible_stats' in \
+                                                                                      dict else False
+
+
+class UserGame:
+    def __init__(self, dict):
+        self.appid = str(dict['appid']).strip()
+        self.playtime_forever = int(str(dict['playtime_forever']).strip())
+        self.playtime_2weeks = int(str(dict['playtime_2weeks']).strip()) if 'playtime_2weeks' in dict else 0
+
+
+class User:
+    def __init__(self, steamid, games):
+        self.games = games
+        self.steamid = steamid
+
+
+class SteamAPI:
+    def __init__(self, apiKey):
+        self.apiKey = apiKey
+        self.num_requests = 0
+
+    # Requests API for steam user games, return json
+    def __request_user_games(self, steamid):
+        try:
+            url = 'http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=%s&steamid=%s&format=json&' \
+                  'include_appinfo=1&include_played_free_games=1' % (self.apiKey, str(steamid))
+            api_wait()
+            self.num_requests += 1
+            resp = requests.get(url).json()['response']
+            # print("resp" + str(type(resp)) + " " + str(resp))
+            return resp['games'] if ('games' in resp) else {}
+        except Exception as e:
+            print("Caught exception in request_user_games")
+            print(e)
+            return {}
+
+    # Take user ID, Request API for friends, return friends request JSON
+    def __request_user_friends(self, steamid):
+        try:
+            url = 'http://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key=%s&steamid=%s' \
+                  '&relationship=friend' % (self.apiKey, str(steamid))
+            api_wait()
+            self.num_requests += 1
+            resp = requests.get(url).json()
+            if 'friendslist' in resp and 'friends' in resp['friendslist']:
+                return resp['friendslist']['friends']
+            return {}
+        except Exception as e:
+            print("Caught exception in request_user_friends")
+            print(e)
+            return {}
+
+    # Return User, {Games} (combined to reduce api calls)
+    def get_user_and_games(self, steamid):
+        data = self.__request_user_games(steamid)
+        # print("data" + str(type(data)) + " " + str(data))
+        games = {}
+        user_games = {}
+        for g in data:
+            # print("game: " + str(type(g)) + " " + str(g))
+            game = Game(g)
+            user_game = UserGame(g)
+            games[game.appid] = game
+            user_games[user_game.appid] = user_game
+        return User(steamid, user_games), games
+
+    def get_friends(self, steamid):
+        data = self.__request_user_friends(steamid)
+        return [Friend(f) for f in data]
---
+++ steam_spider.py
@@ -0,0 +1,80 @@
+import requests
+import sys
+import json
+import os
+
+import steam_api
+
+
+class SteamCrawler:
+    def __init__(self, file_api_key, file_init_ids, file_checked_ids, dir_games, dir_users):
+        with open(file_api_key, 'r+') as f:
+            self.steam = steam_api.SteamAPI(f.readline())
+        self.queue = [e.strip() for e in open(file_init_ids, 'r+').readlines()]
+        self.checked = [e.strip() for e in open(file_checked_ids, 'r+').readlines()]
+
+        self.games = self.__load_games(dir_games)
+        self.users = self.__load_users(dir_users)
+
+        self.checked_out = open(file_checked_ids, 'ab', buffering=0)
+        self.dir_games = dir_games
+        self.dir_users = dir_users
+
+    def __load_games(self, dir_games):
+        dat = {}
+        for f in os.listdir(dir_games):
+            game = steam_api.Game(json.load(open(os.path.join(dir_games, f), 'r+')))
+            dat[game.appid] = game
+        return dat
+
+    def __apply_game(self, game):
+        self.games[game.appid] = game
+        json.dump(game, open(os.path.join(self.dir_games, str(game.appid)), 'w+'), default=lambda o: o.__dict__)
+
+    def __load_users(self, dir_users):
+        usrs = {}
+        for f in os.listdir(dir_users):
+            dat = json.load(open(os.path.join(dir_users, f), 'r+'))
+            usr_games = {}
+            for (appid, g) in dat['games'].items():
+                game = steam_api.UserGame(g)
+                usr_games[appid] = game
+            usr = steam_api.User(dat['steamid'], usr_games)
+            usrs[usr.steamid] = usr
+        return usrs
+
+    def __apply_user(self, user):
+        self.users[user.steamid] = user
+        json.dump(user, open(os.path.join(self.dir_users, str(user.steamid)), 'w+'), default=lambda o: o.__dict__)
+
+    def crawl(self):
+        while len(self.queue) > 0:
+            id = self.queue.pop(0)
+            print("\t - " + str(id))
+            if id in self.checked:
+                print("\t\tAlready searched " + id)
+            else:
+                self.checked.append(id)
+                self.checked_out.write((id + "\n").encode("utf8"))
+                if len(self.queue) < 1000000:
+                    friends = self.steam.get_friends(id)
+                    if len(friends) > 0:
+                        print("\t\t\t" + str(len(friends)) + " friends")
+                        self.queue.extend([friend.steamid for friend in friends])
+                dat = self.steam.get_user_and_games(id)
+                print("\t\t\t" + str(len(dat[1])) + " games")
+                # print("dat1: " + str(type(dat[1])) + " " + str(dat[1]))
+                for (k, g) in dat[1].items():
+                    # print("k: " + str(type(k)) + " " + str(k))
+                    # print("g: " + str(type(g)) + " " + str(g))
+                    self.__apply_game(g)
+                self.__apply_user(dat[0])
+                print("\t\t" + str(len(self.users)) + " total users")
+                print("\t\t" + str(len(self.games)) + " total games")
+                print("\t\t" + str(self.steam.num_requests) + " total requests")
+        self.checked_out.close()
+
+
+if __name__ == "__main__":
+    crawler = SteamCrawler('apikey.txt', 'initids.txt', 'checked.txt', 'games', 'users')
+    crawler.crawl()
